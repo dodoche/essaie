c05879add4e8221023b16356962b70392eb41838
dodoche <doralyfr@yahoo.fr>
2020-02-23 17:07:09 +0100

    Delete run_sample_playbook.sh

--- ansible/run_sample_playbook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z "$1" ]]; then
-    echo "Specify the hostname of a bastion with oc/kubeconfig ready for use."
-    echo "Example: $0 my.bastion.hostname"
-    exit 1
-fi
-
-ansible-playbook -vvvv sample_playbook.yml -i $1,
