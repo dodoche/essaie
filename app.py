822dc8dfca8d0c95c8973a6a7b19958be04fda15
dodoche <doralyfr@yahoo.fr>
2020-02-25 01:46:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from __future__ import print_function
-import time
-import kubernetes.client
 from kubernetes.client.rest import ApiException
 from pprint import pprint
 
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 configuration = kubernetes.client.Configuration()
-api_instance = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(configuration))
+
+config.load_kube_config()
+
+api_instance = client.CoreV1Api()
 name = "licence" # str | name of the ConfigMap
 namespace = 'default' # str | object name and auth scope, such as for teams and projects
 pretty = 'pretty_example' # str | If 'true', then the output is pretty printed. (optional)
@@ -13,7 +15,7 @@ exact = True # bool | Should the export be exact.  Exact export maintains cluste
 export = True # bool | Should this value be exported.  Export strips fields that a user can not specify. Deprecated. Planned for removal in 1.18. (optional)
 
 try:
-     api_response = api_instance.read_namespaced_config_map(name, namespace, pretty=pretty, exact=exact, export=export)
-     pprint(api_response)
+    api_response = api_instance.read_namespaced_config_map(name, namespace, pretty=pretty, exact=exact, export=export)
+    pprint( api_response.data)
 except ApiException as e:
-     print("Exception when calling CoreV1Api->read_namespaced_config_map: %s\n" % e)
+    pprint("Exception when calling CoreV1Api->read_namespaced_config_map: %s\n" % e)
