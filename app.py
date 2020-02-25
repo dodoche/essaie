6a9065d320fe22e6f172f453ba545ce4269490d7
dodoche <doralyfr@yahoo.fr>
2020-02-25 10:57:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from kubernetes.client.rest import ApiException
+from pprint import pprint
+from kubernetes import client, config
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
 
