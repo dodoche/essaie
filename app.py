ec681a4b68c3672d1c07fcb4fc9ad3c7ca99bcaf
dodoche <doralyfr@yahoo.fr>
2020-02-25 09:38:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,28 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def hello(
+def create_app()
 
 
 #configuration = kubernetes.client.Configuration()
 
 #config.load_kube_config()
 
-  api_instance = client.CoreV1Api()
-  name = str(licence) # str | name of the ConfigMap
-  namespace = 'default' # str | object name and auth scope, such as for teams and projects
-  pretty = 'pretty_example' # str | If 'true', then the output is pretty printed. (optional)
-  exact = True # bool | Should the export be exact.  Exact export maintains cluster-specific fields like 'Namespace'. Deprecated. Planned for removal in 1.18. (optional)
-  export = True # bool | Should this value be exported.  Export strips fields that a user can not specify. Deprecated. Planned for removal in 1.18. (optional)
+    api_instance = client.CoreV1Api()
+    name = 'licence' # str | name of the ConfigMap
+    namespace = 'default' # str | object name and auth scope, such as for teams and projects
+    pretty = 'pretty_example' # str | If 'true', then the output is pretty printed. (optional)
+    exact = True # bool | Should the export be exact.  Exact export maintains cluster-specific fields like 'Namespace'. Deprecated. Planned for removal in 1.18. (optional)
+    export = True # bool | Should this value be exported.  Export strips fields that a user can not specify. Deprecated. Planned for removal in 1.18. (optional)
 
-  try:
-      api_response = api_instance.read_namespaced_config_map(name, namespace, pretty=pretty, exact=exact, export=export)
-      pprint( api_response.data)
-  except ApiException as e:
-      pprint("Exception when calling CoreV1Api->read_namespaced_config_map: %s\n" % e)
-   )
+    try:
+        api_response = api_instance.read_namespaced_config_map(name, namespace, pretty=pretty, exact=exact, export=export)
+        pprint( api_response.data)
+    except ApiException as e:
+        pprint("Exception when calling CoreV1Api->read_namespaced_config_map: %s\n" % e)
+      
+   
+   
  
   if __name__ == "__main__":
       application.run()
